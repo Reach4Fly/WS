e06023e857945a96632b06bf8d4f63994ef2de77
Reach4Fly <65672802+Reach4Fly@users.noreply.github.com>
2020-05-21 23:50:01 +0300

    Add files via upload

---
+++ Gyms.WebService/InfrastructureServices/Gateways/Database/GymContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Gyms.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gyms.InfrastructureServices.Gateways.Database
+{
+    public class GymContext : DbContext
+    {
+        public DbSet<GymPoint> GymPoints { get; set; }
+
+        public GymContext(DbContextOptions options)
+            : base(options)
+        { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            FillTestData(modelBuilder);
+        }
+        private void FillTestData(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<GymPoint>().HasData(
+                new
+  {
+                    Id = 1L,
+                    NameZone = "зал тренажерный",
+                    District = "Административный округ: Восточный административный округ",
+                    Area = "район Богородское",
+                    Address = "Краснобогатырская улица, дом 2, строение 1 ",
+                    Email = "",
+                    WebSite = "fitnes.ru"
+
+                },
+                new
+                {
+                    Id = 2L,
+                    NameZone = "зал тренажерный",
+                    District = "Административный округ: Восточный административный округ  ",
+                    Area = "район Вешняки",
+                    Address = "Вешняковская улица, дом 29Д",
+                    Email = "mu_sdc@mail.ru",
+                    WebSite = "sport-vesh.ru "
+                }
+                
+            );
+        }
+    }
+}
---
+++ Gyms.WebService/Migrations/20200521182645_Initial.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gyms.WebService.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GymPoints",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NameObject = table.Column<string>(nullable: true),
+                    NameZone = table.Column<string>(nullable: true),
+                    District = table.Column<string>(nullable: true),
+                    Area = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    WebSite = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GymPoints", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "GymPoints",
+                columns: new[] { "Id", "Address", "Area", "District", "Email", "NameObject", "NameZone", "WebSite" },
+                values: new object[] { 1L, "Краснобогатырская улица, дом 2, строение 1 ", "район Богородское", "Административный округ: Восточный административный округ", "", null, "зал тренажерный", "fitnes.ru" });
+
+            migrationBuilder.InsertData(
+                table: "GymPoints",
+                columns: new[] { "Id", "Address", "Area", "District", "Email", "NameObject", "NameZone", "WebSite" },
+                values: new object[] { 2L, "Вешняковская улица, дом 29Д", "район Вешняки", "Административный округ: Восточный административный округ  ", "mu_sdc@mail.ru", null, "зал тренажерный", "sport-vesh.ru " });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GymPoints");
+        }
+    }
+}
