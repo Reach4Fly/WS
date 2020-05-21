4c72ff2eb694b601f099a4fa4bec4fa93c218964
Reach4Fly <65672802+Reach4Fly@users.noreply.github.com>
2020-05-21 23:46:25 +0300

    Add files via upload

---
+++ SharedComponents/Gyms.BusinessLogic/DomainObjects/Ports/IGymPointRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace Gyms.DomainObjects.Ports
+{
+    public interface IReadOnlyGymPointRepository
+    {
+        Task<GymPoint> GetGymPoint(long id);
+
+        Task<IEnumerable<GymPoint>> GetAllGymPoints();
+
+        Task<IEnumerable<GymPoint>> QueryGymPoints(ICriteria<GymPoint> criteria);
+
+    }
+
+    public interface IGymPointRepository
+    {
+        Task AddGymPoint(GymPoint gympoint);
+
+        Task RemoveGymPoint(GymPoint gympoint);
+
+        Task UpdateGymPoint(GymPoint gympoint);
+    }
+}
---
+++ SharedComponents/Gyms.GymPointListQuery/ApplicationServices/GetMedPointListUseCase/GetGymPointListUseCaseRequest.cs
@@ -0,0 +1,30 @@
+﻿using Gyms.ApplicationServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gyms.ApplicationServices.GetGymPointListUseCase
+{
+    public class GetGymPointListUseCaseRequest : IUseCaseRequest<GetGymPointListUseCaseResponse>
+    {
+        public string District { get; private set; }
+        public long? GymPointId { get; private set; }
+
+        private GetGymPointListUseCaseRequest()
+        { }
+
+        public static GetGymPointListUseCaseRequest CreateAllMedPointsRequest()
+        {
+            return new GetGymPointListUseCaseRequest();
+        }
+
+        public static GetGymPointListUseCaseRequest CreateMedPointRequest(long gympointId)
+        {
+            return new GetGymPointListUseCaseRequest() { GymPointId = gympointId };
+        }
+        public static GetGymPointListUseCaseRequest CreateDistrictGymPointsRequest(string district)
+        {
+            return new GetGymPointListUseCaseRequest() { District = district };
+        }
+    }
+}
